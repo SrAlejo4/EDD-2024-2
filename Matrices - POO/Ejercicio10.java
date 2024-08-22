ac3d7a069f1a513946e0f8f57beb8c91ec9e1fc6
SrAlejo4 <alejandrogarci087@gmail.com>
2024-08-22 09:27:54 -0500

    Se termina ejercicio 10

--- Matrices - POO/Ejercicio10.java
+++ Matrices - POO/Ejercicio10.java
@@ -22,6 +22,15 @@ public class Ejercicio10 {
         }
     }
 
+    // Método para llenar la matriz con la cantidad de dígitos de la matriz de Strings ...
+    public void matrizNumeros(){
+        for(int i = 0; i < this.matrizNum.length; i++){
+            for(int j = 0; j < this.matrizNum[i].length; j++){
+                this.matrizNum[i][j] = this.matrizText[i][j].length();
+            }
+        }
+    }
+
     public void imprimirMatrices(){
         // Imprimimos la matriz de STRINGS ...
         for(int i = 0; i < this.matrizText.length; i++){
@@ -32,18 +41,26 @@ public class Ejercicio10 {
         }
 
         // Imprimimos la matriz de dígitos ...
+        System.out.println(" ");
+        for(int i = 0; i < this.matrizNum.length; i++){
+            for(int j = 0; j < this.matrizNum[i].length; j++){
+                System.out.print(" " + this.matrizNum[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Digite la cantidad de filas de la matriz: ");
+        System.out.print("\nDigite la cantidad de filas de la matriz: ");
         int filas = entrada.nextInt();
-        System.out.print("\nDigite la cantidad de columnas de la matriz: ");
+        System.out.print("Digite la cantidad de columnas de la matriz: ");
         int columnas = entrada.nextInt();
 
         Ejercicio10 prueba = new Ejercicio10(filas, columnas);
         prueba.llenarMatriz();
+        prueba.matrizNumeros();
+        System.out.println("\n ---------- MATRICES ---------- ");
         prueba.imprimirMatrices();
-
     }
 }
